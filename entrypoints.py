d0074eaf3bb9326b4fb48e19aacccd96b8c78745
Thomas Kluyver <thomas@kluyver.me.uk>
2016-05-21 10:29:01 +0100

    Import configparser from backports on Python 2
    Closes gh-3

--- entrypoints.py
+++ entrypoints.py
@@ -1,5 +1,4 @@
 """Discover and load entry points from installed packages."""
-import configparser
 from contextlib import contextmanager
 import glob
 from importlib import import_module
@@ -10,6 +9,11 @@ import re
 import sys
 import warnings
 import zipfile
+
+if sys.version_info[0] >= 3:
+    import configparser
+else:
+    from backports import configparser
 
 entry_point_pattern = re.compile(r"""
 (?P<modulename>\w+(\.\w+)*)
