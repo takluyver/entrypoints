52ee19ec70a6ad6cfd47c57a88724674f0eec4fe
Doug Hellmann <doug@doughellmann.com>
2020-07-03 17:38:56 -0400

    add a caching layer with a single cache
    This experimental patch builds on the existing caching work. It adds a
    new module that should be API-compatible with the base module (as
    demonstrated by the test file).
    
    The cache stores the parsed text data from all of the ini input files
    in a single JSON file with a name based on the hash of the path
    entries and the mtimes. This should produce a unique filename for each
    import path, regardless of the use of a virtualenv.
    
    The data is stored in a format that means no other files need to be
    examined or parsed in order to return EntryPoint objects.
    
    Addresses #16
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ entrypoints_cache.py
@@ -0,0 +1,145 @@
+"""Use a cache layer in front of entry point scanning."""
+
+import errno
+import hashlib
+import json
+import logging
+import os
+import os.path
+import struct
+import sys
+
+import entrypoints
+
+NoSuchEntryPoint = entrypoints.NoSuchEntryPoint
+BadEntryPoint = entrypoints.BadEntryPoint
+EntryPoint = entrypoints.EntryPoint
+
+
+log = logging.getLogger('entrypoints_cache')
+
+
+def _get_cache_dir():
+    """Locate a platform-appropriate cache directory to use.
+
+    Does not ensure that the cache directory exists.
+    """
+    # Linux, Unix, AIX, etc.
+    if os.name == 'posix' and sys.platform != 'darwin':
+        # use ~/.cache if empty OR not set
+        return os.environ.get("XDG_CACHE_HOME", None) \
+              or os.path.expanduser('~/.cache/python-entrypoints')
+
+    # Mac OS
+    elif sys.platform == 'darwin':
+        return os.path.expanduser('~/Library/Caches/Python Entry Points')
+
+    # Windows (hopefully)
+    else:
+        return os.environ.get('LOCALAPPDATA', None) \
+               or os.path.expanduser('~\\AppData\\Local\\Python Entry Points')
+
+
+def _hash_settings_for_path(path):
+    """Return a hash and the path settings that created it.
+    """
+    paths = []
+    stat = os.stat
+    h = hashlib.sha1()
+    for entry in path:
+        h.update(entry.encode('utf-8'))
+        try:
+            s = stat(entry)
+        except OSError as err:
+            if err.errno == errno.ENOENT:
+                continue
+            raise
+        else:
+            paths.append((entry, s.st_mtime))
+            h.update(struct.Struct('f').pack(s.st_mtime))
+
+    return (h.hexdigest(), paths)
+
+
+def _build_cacheable_data(path):
+    groups = {}
+    for config, distro in entrypoints.iter_files_distros(path=path):
+        for group_name, group_val in config.items():
+            groups.setdefault(group_name, []).extend(
+                (name, epstr, distro.name, distro.version)
+                for name, epstr in group_val.items()
+            )
+    return {'groups': groups}
+
+
+class Cache:
+
+    def __init__(self, cache_dir=None):
+        if cache_dir is None:
+            cache_dir = _get_cache_dir()
+        self._dir = cache_dir
+
+    def _get_data_for_path(self, path):
+        if path is None:
+            path = sys.path
+        digest, path_values = _hash_settings_for_path(path)
+        filename = os.path.join(self._dir, digest)
+        try:
+            log.debug('reading %s', filename)
+            with open(filename, 'r') as f:
+                data = json.load(f)
+        except (IOError, json.JSONDecodeError):
+            data = _build_cacheable_data(path)
+            data['path_values'] = path_values
+            try:
+                log.debug('writing to %s', filename)
+                os.makedirs(self._dir, exist_ok=True)
+                with open(filename, 'w') as f:
+                    json.dump(data, f)
+            except (IOError, OSError):
+                # Could not create cache dir or write file.
+                pass
+        return data
+
+    def get_group_all(self, group, path=None):
+        result = []
+        data = self._get_data_for_path(path)
+        group_data = data.get('groups', {}).get(group, [])
+        for name, epstr, distro_name, distro_version in group_data:
+            distro = entrypoints.Distribution(distro_name, distro_version)
+            with BadEntryPoint.err_to_warnings():
+                result.append(EntryPoint.from_string(epstr, name, distro))
+        return result
+
+    def get_group_named(self, group, path=None):
+        result = {}
+        for ep in self.get_group_all(group, path=path):
+            if ep.name not in result:
+                result[ep.name] = ep
+        return result
+
+    def get_single(self, group, name, path=None):
+        data = self._get_data_for_path(path)
+        group_data = data.get('groups', {}).get(group, [])
+        for name, epstr, distro_name, distro_version in group_data:
+            if name == name:
+                distro = entrypoints.Distribution(distro_name, distro_version)
+                with BadEntryPoint.err_to_warnings():
+                    return EntryPoint.from_string(epstr, name, distro)
+        raise NoSuchEntryPoint(group,  name)
+
+
+_c = Cache()
+get_group_all = _c.get_group_all
+get_group_named = _c.get_group_named
+get_single = _c.get_single
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        stream=sys.stderr,
+        level=logging.DEBUG,
+    )
+    for ep in get_group_all(sys.argv[1]):
+        print(ep)
+        ep.load()
